625b80133a4b39dbb0cd6747e96a90d0c1856062
rii922 <147631072+rii922@users.noreply.github.com>
2024-04-16 21:14:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from keep_alive import keep_alive
+import urllib.parse
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,9 +13,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("greet"):
-        await message.channel.send("おはよう")
+    if message.content.startswith("math "):
+        tex = urllib.parse.quote(message.content[5:])
+        url = "https://api.excelapi.org/math/tex2image?math=" + tex
+        await message.channel.send(url)
     if message.content == "exit":
+        await message.channel.send("ばいばーい")
         exit()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
