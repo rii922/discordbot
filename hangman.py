c5f5ca355491f3b3715e2e969ecbaa83cbe75ec4
rii922 <147631072+rii922@users.noreply.github.com>
2024-05-05 11:19:01 +0900

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,12 +1,76 @@
 import random
+import requests
+import asyncio
 
 words = []
 
 def init():
-	f = open("hangman_words.txt", "r")
-	global words
-	words = f.read().split()
-	f.close()
+    f = open("hangman_words.txt", "r")
+    global words
+    words = f.read().split()
+    f.close()
 
 def choose_word():
-	return random.choice(words)
+    return random.choice(words)
+
+async def play(client, message):
+    word = choose_word()
+    life = len(word)
+    opened = [False for _ in range(len(word))]
+    chars = []
+    try:
+        data = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word).json()[0]
+    except:
+        data = None
+    description = "**" + word
+    if "phonetic" in data:
+        description += "** " + data["phonetic"]
+    else:
+        description += "(phonetic not found)"
+    if "meanings" in data:
+        for meaning in data["meanings"]:
+            description += "\n__" + meaning["partOfSpeech"] + "__"
+            for definition in meaning["definitions"]:
+                description += "\n- " + definition["definition"]
+    else:
+        description += "(meanings not found)"
+    if "sourceUrls" in data:
+        description += "\nsource: " + " ".join(data["sourceUrls"])
+    else:
+        description += "source not found"
+    await message.channel.send("__**hangman**__\n英単語を当てよう！\n答え方:\n- アルファベット1文字を開ける\n- 単語を丸ごと答える")
+    def check(ans_message):
+        if ans_message.channel != message.channel:
+            return False
+        for c in ans_message.content:
+            if not ("a" <= c <= "z" or "A" <= c <= "Z"):
+                return False
+        return True
+    while life > 0:
+        await message.channel.send("現在の状態: **" + " ".join([(word[i] if opened[i] else "\\_") for i in range(len(word))]) + "**\n残機: " + str(life) + "\n使った文字: " + " ".join(chars))
+        try:
+            ans_message = await client.wait_for("message", check=check, timeout=180)
+            if len(ans_message.content) == 1:
+                char = ans_message.content.lower()
+                for i in range(len(word)):
+                    if word[i] == char:
+                        opened[i] = True
+                if char in word:
+                    chars.append("**" + char + "**")
+                else:
+                    chars.append(char)
+                    life -= 1
+            else:
+                predict = ans_message.content.lower()
+                if predict == word:
+                    await message.channel.send(ans_message.author.mention + "正解！(**" + word + "**)")
+                    await ans_message.add_reaction("👍")
+                    break
+                else:
+                    life -= 1
+        except asyncio.TimeoutError:
+            await message.channel.send("3分間無言だったので終了するよ\n正解は**" + word + "**でした！")
+            break
+    if life == 0:
+        await message.channel.send("残念...\n正解は**" + word + "**でした！")
+    await message.channel.send(description)
--- tinitu.py
+++ tinitu.py
@@ -1,76 +1,115 @@
 import copy
 import random
+import asyncio
 
 def is_winning_hand(hand, meld=4, eye=1):
-	if meld == 0 and eye == 0:
-		return True
-	if meld > 0:
-		for i in range(9):
-			if hand[i] >= 3:
-				nhand = copy.copy(hand)
-				nhand[i] -= 3
-				if is_winning_hand(nhand, meld-1, eye):
-					return True
-		for i in range(7):
-			if hand[i] >= 1 and hand[i+1] >= 1 and hand[i+2] >= 1:
-				nhand = copy.copy(hand)
-				nhand[i] -= 1
-				nhand[i+1] -= 1
-				nhand[i+2] -= 1
-				if is_winning_hand(nhand, meld-1, eye):
-					return True
-	if eye > 0:
-		for i in range(9):
-			if hand[i] >= 2:
-				nhand = copy.copy(hand)
-				nhand[i] -= 2
-				if is_winning_hand(nhand, meld, eye-1):
-					return True
-	return False
+    if meld == 0 and eye == 0:
+        return True
+    if meld > 0:
+        for i in range(9):
+            if hand[i] >= 3:
+                nhand = copy.copy(hand)
+                nhand[i] -= 3
+                if is_winning_hand(nhand, meld-1, eye):
+                    return True
+        for i in range(7):
+            if hand[i] >= 1 and hand[i+1] >= 1 and hand[i+2] >= 1:
+                nhand = copy.copy(hand)
+                nhand[i] -= 1
+                nhand[i+1] -= 1
+                nhand[i+2] -= 1
+                if is_winning_hand(nhand, meld-1, eye):
+                    return True
+    if eye > 0:
+        for i in range(9):
+            if hand[i] >= 2:
+                nhand = copy.copy(hand)
+                nhand[i] -= 2
+                if is_winning_hand(nhand, meld, eye-1):
+                    return True
+    return False
 
 def add_random_chow(hand):
-	chow_list = []
-	for i in range(7):
-		if hand[i] <= 3 and hand[i+1] <= 3 and hand[i+2] <= 3:
-			chow_list.append(i)
-	chow_begin = random.choice(chow_list)
-	for i in range(3):
-		hand[chow_begin+i] += 1
+    chow_list = []
+    for i in range(7):
+        if hand[i] <= 3 and hand[i+1] <= 3 and hand[i+2] <= 3:
+            chow_list.append(i)
+    chow_begin = random.choice(chow_list)
+    for i in range(3):
+        hand[chow_begin+i] += 1
 
 def add_random_pong(hand):
-	pong_list = []
-	for i in range(9):
-		if hand[i] <= 1:
-			pong_list.append(i)
-	pong = random.choice(pong_list)
-	hand[pong] += 3
+    pong_list = []
+    for i in range(9):
+        if hand[i] <= 1:
+            pong_list.append(i)
+    pong = random.choice(pong_list)
+    hand[pong] += 3
 
 def add_random_eye(hand):
-	eye_list = []
-	for i in range(9):
-		if hand[i] <= 2:
-			eye_list.append(i)
-	eye = random.choice(eye_list)
-	hand[eye] += 2
+    eye_list = []
+    for i in range(9):
+        if hand[i] <= 2:
+            eye_list.append(i)
+    eye = random.choice(eye_list)
+    hand[eye] += 2
 
 def generate_quiz():
-	hand = [0 for _ in range(9)]
-	for _ in range(4):
-		if random.randrange(4) == 0:
-			add_random_pong(hand)
-		else:
-			add_random_chow(hand)
-	add_random_eye(hand)
-	eliminate_list = []
-	for i in range(9):
-		if hand[i] > 0:
-			eliminate_list.append(i)
-	hand[random.choice(eliminate_list)] -= 1
-	ans = set()
-	for i in range(9):
-		if hand[i] <= 3:
-			nhand = copy.copy(hand)
-			nhand[i] += 1
-			if is_winning_hand(nhand):
-				ans.add(i)
-	return (hand, ans)
+    hand = [0 for _ in range(9)]
+    for _ in range(4):
+        if random.randrange(4) == 0:
+            add_random_pong(hand)
+        else:
+            add_random_chow(hand)
+    add_random_eye(hand)
+    eliminate_list = []
+    for i in range(9):
+        if hand[i] > 0:
+            eliminate_list.append(i)
+    hand[random.choice(eliminate_list)] -= 1
+    ans = set()
+    for i in range(9):
+        if hand[i] <= 3:
+            nhand = copy.copy(hand)
+            nhand[i] += 1
+            if is_winning_hand(nhand):
+                ans.add(i)
+    return (hand, ans)
+
+async def play(client, message):
+    hand, ans = generate_quiz()
+    hand_list = []
+    for i in range(9):
+        hand_list += [str(i+1) for _ in range(hand[i])]
+    hand_str = " ".join(hand_list)
+    random.shuffle(hand_list)
+    hand_str_shuffled = " ".join(hand_list)
+    ans_list = [str(wait+1) for wait in ans]
+    ans_list.sort()
+    await message.channel.send("__**清一色何待ちクイズ**__\nこの清一色、何待ち？\n**" + hand_str_shuffled + "**")
+    def check(ans_message):
+        if ans_message.channel != message.channel:
+            return False
+        user_ans = set()
+        for i in range(9):
+            if str(i+1) in ans_message.content:
+                user_ans.add(i)
+        return user_ans == ans
+    async def send_correct_message(ans_message):
+        await ans_message.channel.send(ans_message.author.mention + "正解！(**" + ", ".join(ans_list) + "**待ち)")
+        await ans_message.add_reaction("👍")
+    try:
+        ans_message = await client.wait_for("message", check=check, timeout=30)
+        await send_correct_message(ans_message)
+    except asyncio.TimeoutError:
+        await message.channel.send("分からない？...じゃあ理牌してあげる～\n**" + hand_str + "**")
+        try:
+            ans_message = await client.wait_for("message", check=check, timeout=30)
+            await send_correct_message(ans_message)
+        except asyncio.TimeoutError:
+            await message.channel.send("まだ分からない？しょうがないなあ...この待ちは**" + str(len(ans)) + "**種あるよ～")
+            try:
+                ans_message = await client.wait_for("message", check=check, timeout=30)
+                await send_correct_message(ans_message)
+            except asyncio.TimeoutError:
+                await message.channel.send("残念...時間切れだよ！\n正解は**" + ", ".join(ans_list) + "**待ちでした！難しかったかな？")
