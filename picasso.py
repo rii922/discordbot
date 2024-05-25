341e17a4876b12a46050ddce3649c7c4ac65af9a
rii922 <147631072+rii922@users.noreply.github.com>
2024-05-25 18:07:47 +0900

    Update picasso.py

--- picasso.py
+++ picasso.py
@@ -1,19 +1,16 @@
 import discord
-from diffusers import StableDiffusionXLPipeline, EulerAncestralDiscreteScheduler
-import torch
+import spaces
+from diffusers import DiffusionPipeline
 
-model_id = "aipicasso/emi-2"
+pipe = DiffusionPipeline.from_pretrained("aipicasso/emi-2")
+pipe.to("cuda")
 
-scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id,subfolder="scheduler")
-pipe = StableDiffusionXLPipeline.from_pretrained(model_id, scheduler=scheduler)
-pipe = pipe.to("cpu")
-
-# prompt = "1girl, upper body, brown bob short hair, brown eyes, looking at viewer, cherry blossom"
-# images = pipe(prompt, num_inference_steps=20).images
-# images[0].save("girl.png")
+@spaces.GPU
+def inference(prompt):
+    return pipe(prompt).images
 
 async def play(client, message):
     prompt = message.content[8:]
-    image = pipe(prompt, num_inference_steps=20).images[0]
+    image = pipe(prompt).images[0]
     image.save("picasso.png")
     await message.channel.send(file=discord.File("picasso.png"))
