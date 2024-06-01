78f91a9caae46f6271b1588b6c32ca0a0341acd8
rii922 <147631072+rii922@users.noreply.github.com>
2024-06-02 01:52:13 +0900

    Use serverless inference API

--- picasso.py
+++ picasso.py
@@ -1,18 +1,22 @@
 import discord
-from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
-import torch
+import os
+import requests
+import io
+from PIL import Image
 
-model_id = "aipicasso/picasso-diffusion-1-1"
-scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler)
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 
 def inference(prompt):
-	prompt = "masterpiece, anime, " + prompt
-	negative_prompt="lowres, kanji,  monochrome, ((bad anatomy)), ((bad hands)), text, missing finger, extra digits, fewer digits, blurry, ((mutated hands and fingers)), (poorly drawn face), ((mutation)), ((deformed face)), (ugly), ((bad proportions)), ((extra limbs)), extra face, (double head), (extra head), ((extra feet)), monster, logo, cropped, jpeg, humpbacked, long body, long neck, ((jpeg artifacts)), ((censored)), ((bad aesthetic))"
-	return pipe(prompt, negative_prompt=negative_prompt).images
+	url = "https://api-inference.huggingface.co/models/cagliostrolab/animagine-xl-3.1"
+	header = { "Authorization": f"Bearer {HUGGINGFACE_TOKEN}" }
+	payload = { "inputs": prompt }
+	result = requests.post(url, headers=header, json=payload).content
+	return result
 
 async def play(client, message):
     prompt = message.content[8:]
-    image = inference(prompt)[0]
-    image.save("picasso.png")
-    await message.channel.send(file=discord.File("picasso.png"))
+    image = Image.open(io.BytesIO(inference(prompt)))
+    bytesio = io.BytesIO()
+    image.save(bytesio, format="png")
+    bytesio.seek(0)
+    await message.channel.send(file=discord.File(fp=bytesio, filename="picasso.png"))
