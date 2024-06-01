676f573833de1e62177aea0c7a515caa0a314211
rii922 <147631072+rii922@users.noreply.github.com>
2024-06-01 20:41:12 +0900

    Update picasso.py

--- picasso.py
+++ picasso.py
@@ -1,13 +1,18 @@
 import discord
+from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
+import torch
 import spaces
-from diffusers import DiffusionPipeline
 
-pipe = DiffusionPipeline.from_pretrained("aipicasso/emi-2")
+model_id = "aipicasso/picasso-diffusion-1-1"
+scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler)
 pipe.to("cuda")
 
 @spaces.GPU
 def inference(prompt):
-    return pipe(prompt).images
+	prompt = "masterpiece, anime, " + prompt
+	negative_prompt="lowres , kanji,  monochrome, ((bad anatomy)), ((bad hands)), text, missing finger, extra digits, fewer digits, blurry, ((mutated hands and fingers)), (poorly drawn face), ((mutation)), ((deformed face)), (ugly), ((bad proportions)), ((extra limbs)), extra face, (double head), (extra head), ((extra feet)), monster, logo, cropped, jpeg, humpbacked, long body, long neck, ((jpeg artifacts)), ((censored)), ((bad aesthetic))"
+	return pipe(prompt, negative_prompt=negative_prompt).images
 
 async def play(client, message):
     prompt = message.content[8:]
