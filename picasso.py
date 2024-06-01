83c94fa982489a114d6adfd98c2419d690580785
rii922 <147631072+rii922@users.noreply.github.com>
2024-06-02 00:01:31 +0900

    Update picasso.py

--- picasso.py
+++ picasso.py
@@ -1,17 +1,14 @@
 import discord
 from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
 import torch
-import spaces
 
 model_id = "aipicasso/picasso-diffusion-1-1"
 scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
 pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler)
-pipe.to("cuda")
 
-@spaces.GPU
 def inference(prompt):
 	prompt = "masterpiece, anime, " + prompt
-	negative_prompt="lowres , kanji,  monochrome, ((bad anatomy)), ((bad hands)), text, missing finger, extra digits, fewer digits, blurry, ((mutated hands and fingers)), (poorly drawn face), ((mutation)), ((deformed face)), (ugly), ((bad proportions)), ((extra limbs)), extra face, (double head), (extra head), ((extra feet)), monster, logo, cropped, jpeg, humpbacked, long body, long neck, ((jpeg artifacts)), ((censored)), ((bad aesthetic))"
+	negative_prompt="lowres, kanji,  monochrome, ((bad anatomy)), ((bad hands)), text, missing finger, extra digits, fewer digits, blurry, ((mutated hands and fingers)), (poorly drawn face), ((mutation)), ((deformed face)), (ugly), ((bad proportions)), ((extra limbs)), extra face, (double head), (extra head), ((extra feet)), monster, logo, cropped, jpeg, humpbacked, long body, long neck, ((jpeg artifacts)), ((censored)), ((bad aesthetic))"
 	return pipe(prompt, negative_prompt=negative_prompt).images
 
 async def play(client, message):
